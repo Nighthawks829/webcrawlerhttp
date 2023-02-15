9b52ed20cc8a0ef7f93e2c7d2c5a0e184eea9579
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2023-02-15 18:32:28 +0800

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,32 @@
+const { normalizeURL } = require("./crawl.js")
+const { test, expect } = require('@jest/globals')
+
+test('normalizeURL', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip trainling slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capital', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip http', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
