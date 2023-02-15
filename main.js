793a333977a03b4abdc000a8735c588a58f927b2
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2023-02-15 19:15:56 +0800

    getURLsFromHTML function and tests

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js")
+
+const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev>
+                Boot.dev Blog
+            </a>
+        </body>
+    </html> 
+    `
+const inputBaseURL = "https://blog.boot.dev"
+
+
+const output = getURLsFromHTML(inputHTMLBody,inputBaseURL)
