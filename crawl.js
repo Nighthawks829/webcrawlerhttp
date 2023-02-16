b16f14096f9b84844343abfe0103573089695fad
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2023-02-16 10:02:01 +0800

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,41 +1,63 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL){
-  console.log(`actively crawing: ${currentURL}`)
-    try{
-      const resp=await fetch(currentURL)
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL)
+  const currentURLObj = new URL(currentURL)
 
-      if(resp.status>399){
-        console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-        return
-      }
-      const contentType=resp.headers.get("content-type")
-      if(!contentType.includes("text/html")){
-        console.log(`non html response, content type: ${contentType} on page ${currentURL}`)
-        return
-      }
-      console.log(await resp.text())
-    }catch(err){
-      console.log(`error in fetch: ${err.message}, on page: ${currentURL} `)
+  if (baseURL.hostname !== currentURL.hostname) {
+    return pages
+  }
+
+  const normalizedURL = normalizeURL(currentURL)
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++
+    return pages
+  }
+
+  pages[normalizedURL] = 1
+
+  console.log(`actively crawing: ${currentURL}`)
+  try {
+    const resp = await fetch(currentURL)
+
+    if (resp.status > 399) {
+      console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+      return pages
     }
+    const contentType = resp.headers.get("content-type")
+    if (!contentType.includes("text/html")) {
+      console.log(`non html response, content type: ${contentType} on page ${currentURL}`)
+      return pages
+    }
+    const htmlBody = await resp.text()
+
+    nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
+  } catch (err) {
+    console.log(`error in fetch: ${err.message}, on page: ${currentURL} `)
+  }
+  return pages
 }
 
 
-function getURLsFromHTML(htmlBody, baseURL){
+function getURLsFromHTML(htmlBody, baseURL) {
   const urls = []
   const dom = new JSDOM(htmlBody)
   const aElements = dom.window.document.querySelectorAll('a')
-  for (const aElement of aElements){
-    if (aElement.href.slice(0,1) === '/'){
+  for (const aElement of aElements) {
+    if (aElement.href.slice(0, 1) === '/') {
       try {
         urls.push(new URL(aElement.href, baseURL).href)
-      } catch (err){
+      } catch (err) {
         console.log(`${err.message}: ${aElement.href}`)
       }
     } else {
       try {
         urls.push(new URL(aElement.href).href)
-      } catch (err){
+      } catch (err) {
         console.log(`${err.message}: ${aElement.href}`)
       }
     }
@@ -43,10 +65,10 @@ function getURLsFromHTML(htmlBody, baseURL){
   return urls
 }
 
-function normalizeURL(url){
+function normalizeURL(url) {
   const urlObj = new URL(url)
   let fullPath = `${urlObj.host}${urlObj.pathname}`
-  if (fullPath.length > 0 && fullPath.slice(-1) === '/'){
+  if (fullPath.length > 0 && fullPath.slice(-1) === '/') {
     fullPath = fullPath.slice(0, -1)
   }
   return fullPath
